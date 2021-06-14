e5aee322b80041bb0215e1e92fd960186dc7063c
barbosound <barbo_94@hotmail.com>
2021-06-14 14:40:56 +0200

    rollback

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,7 +2,7 @@ const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
-module.exports = db = {User};
+module.exports = db = {};
 
 initialize();
 
