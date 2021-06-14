310fd21efe494d066e5ba503a44ccb84c28cd097
barbosound <barbo_94@hotmail.com>
2021-06-14 14:02:26 +0200

    remove test

--- _helpers/db.js
+++ _helpers/db.js
@@ -9,11 +9,11 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host: "robinbook.cvzz91ztbary.eu-west-3.rds.amazonaws.com", port:3306, user:"admin", password:"RobinBookDB" });
+    const connection = await mysql.createConnection({ host, port, user, password });
     // await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize("robinbook", user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
