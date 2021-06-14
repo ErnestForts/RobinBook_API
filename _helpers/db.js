e93ced58af730e03a20badff1386719c912e5f73
barbosound <barbo_94@hotmail.com>
2021-06-14 14:38:30 +0200

    test

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,7 +2,7 @@ const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
-module.exports = db = {};
+module.exports = db = {User};
 
 initialize();
 
