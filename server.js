49822f29e0adac2c2eebbf0dbb2d6bde2a5c76f4
ErnestForts <ernest.forts@eram.cat>
2021-06-14 13:12:02 +0200

    user psaword y aws config

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ require('rootpath')();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const errorHandler = require('_middleware/error-handler');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 // api routes
