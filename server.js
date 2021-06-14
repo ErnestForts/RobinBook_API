7dd69243ad889b79baa2581389a8e5a54abf04df
Héctor Realpe <hrealpe@gmail.com>
2021-06-14 20:16:10 +0200

    endpoint place LISTO!

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.get('/', (req, res) => {
     res.send(respuesta)
 });
 
+/********************************** BOOK *******************************/
+
 app.get('/book', (req, res) => {
     const id = req.body.libro_id;
     if(id) {
@@ -68,7 +70,7 @@ app.post('/book', (req, res) => {
 
 app.put('/book', (req, res) => {
     let params = [req.body.Titulo, req.body.Autor, req.body.Descripcion, req.body.Foto, req.body.libro_id];
-    let sql = 'UPDATE robinbook.Libros AS book SET book.Titulo=COALESCE(?, Titulo), book.Autor=COALESCE(?, Autor), book.Descripcion=COALESCE(?, Descripcion) , book.Foto=COALESCE(?, Foto) WHERE (libro_id = ?);';
+    let sql = 'UPDATE robinbook.Libros AS book SET book.Titulo=COALESCE(?, Titulo), book.Autor=COALESCE(?, Autor), book.Descripcion=COALESCE(?, Descripcion), book.Foto=COALESCE(?, Foto) WHERE (libro_id = ?);';
     connection.query(sql, params, (err, result) => {
         if (err) throw res.send(err);
         res.send(result);
@@ -84,13 +86,56 @@ app.delete('/book', (req, res) => {
     })
 });
 
+/********************************** PLACE *******************************/
+
+app.get('/place', (req, res) => {
+    const id = req.body.Lugar_id;
+    if(id) {
+        let sql = `SELECT * FROM robinbook.Lugares WHERE Lugar_id = ${id};`;
+        connection.query(sql, id, (err,result) => {
+            if(err) throw res.send(err);
+            else res.send(result);
+        });
+    } else {
+        let sql = 'SELECT * FROM robinbook.Lugares;';
+        connection.query(sql, (err, result) => {
+            if(err) throw res.send(err);
+            res.send(result);
+        });
+    }
+});
+
+app.post('/place', (req, res) => {
+    let params = [req.body.Nombre, req.body.Descripcion, req.body.Foto, req.body.tieneLibro];
+    let sql = 'INSERT INTO robinbook.Lugares (Nombre, Descripcion, Foto, tieneLibro) VALUES (?,?,?,?);';
+    connection.query(sql, params, (err, result) => {
+        if(err) throw res.send(err);
+        res.send(result);
+    });
+});
+
+app.put('/place', (req, res) => {
+    let params = [req.body.Nombre, req.body.Descripcion, req.body.Foto, req.body.tieneLibro, req.body.Lugar_id];
+    let sql = 'UPDATE robinbook.Lugares AS place SET place.Nombre=COALESCE(?, Nombre), place.Descripcion=COALESCE(?, Descripcion), place.Foto=COALESCE(?, Foto),place.tieneLibro=COALESCE(?, tieneLibro) WHERE (Lugar_id = ?);';
+    connection.query(sql, params, (err, result) => {
+        if (err) throw res.send(err);
+        res.send(result);
+    });
+});
+
+app.delete('/place', (req, res) => {
+    let params = [req.body.Lugar_id];
+    let sql = "DELETE FROM robinbook.Lugares WHERE (Lugar_id = ?);";
+    connection.query(sql, params, (err, result) => {
+        if (err) throw res.send(err);
+        res.send(result);
+    })
+});
 
 app.use((req, res) => {
     respuesta = { error:true, codigo: 200, mensaje: 'URL not found'};
     res.status(404).send(respuesta);
 });
-
-
 
 // app.listen(port, () => {
 //     console.log('App listening on port 300');
